74fba4b98958718455d687ad74d842a057aefd6c
yangbashan <15733097205@163.com>
2019-04-19 15:44:52 +0800

    use vuex

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -17,10 +17,11 @@
 </template>
 
 <script>
+import { mapState } from 'vuex'
 export default {
   name: 'HomeHeader',
-  props: {
-    city: String
+  computed: {
+    ...mapState(['city'])
   }
 }
 </script>
@@ -33,28 +34,29 @@ export default {
         background: $bgColor
         color: #fff
         .header-left
-            width: .64rem
-            float: left
-            .back-icon
-                width .64rem
-                text-align: center
-                font-size: .4rem
-        .header-input
-            flex: 1
-            height: .64rem
-            line-height: .64rem
-            margin-top: .12rem
-            margin-left: .2rem
-            padding-left: .2rem
-            background: #fff
-            border-radius: .1rem
-            color: #ccc
-        .header-right
-            width: 1.24rem
-            float: right
+          width: .64rem
+          float: left
+          .back-icon
+            width .64rem
             text-align: center
-            color: #fff
-            .arrow-icon
-                font-size: .24rem
-                margin-left: -.04rem
+            font-size: .4rem
+        .header-input
+          flex: 1
+          height: .64rem
+          line-height: .64rem
+          margin-top: .12rem
+          margin-left: .2rem
+          padding-left: .2rem
+          background: #fff
+          border-radius: .1rem
+          color: #ccc
+        .header-right
+          min-width: 1.04rem
+          padding: 0 .1rem
+          float: right
+          text-align: center
+          color: #fff
+          .arrow-icon
+            font-size: .24rem
+            margin-left: -.04rem
 </style>
